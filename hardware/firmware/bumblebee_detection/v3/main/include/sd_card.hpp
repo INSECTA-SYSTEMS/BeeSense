26e8b7d196253e94a5bf4fce3db5e41c5bd57cfa
Nils GB <nilsgrossebeikel@gmail.com>
2026-01-22 16:38:08 +0100

    feat: Add timestamp-based image storage and increase app partition

--- hardware/firmware/bumblebee_detection/v3/main/include/sd_card.hpp
+++ hardware/firmware/bumblebee_detection/v3/main/include/sd_card.hpp
@@ -2,6 +2,7 @@
 
 #include "dl_image_define.hpp"
 #include "dl_cls_postprocessor.hpp"  // for dl::cls::result_t
+#include <string>
 
 namespace sdcard {
 
@@ -12,6 +13,8 @@ bool create_dir(const char *full_path);
 int count_files(const char *full_path);
 
 bool save_detected_jpeg(const dl::image::img_t &img, const dl::cls::result_t &best, const char *dir_full_path);
-bool save_classified_jpeg(const dl::image::img_t &img, const dl::cls::result_t &best, const char *dir_full_path);
+
+// Generate timestamp-based filename (YYYYMMDD_HHMMSS format)
+std::string get_timestamp_filename(const char *dir_path, const char *prefix, const char *extension);
 
 } // namespace sdcard
